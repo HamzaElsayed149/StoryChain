5efe203e5dda9880886d6672cb6d2b849131c435
hamza elsayed <hamzaelsayed255@gmail.com>
2025-04-24 22:29:03 +0200

    Update StoriesContext.jsx

--- FrontEnd/src/context/StoriesContext.jsx
+++ FrontEnd/src/context/StoriesContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 
 const StoriesContext = createContext();
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://story-chain-jade.vercel.app/api';
 
 export const StoriesProvider = ({ children }) => {
   const [stories, setStories] = useState([]);
