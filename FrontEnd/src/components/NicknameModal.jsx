276fe56f438c5696eb5448161f6aa8896d7981fd
hamza elsayed <hamzaelsayed255@gmail.com>
2025-05-09 00:17:27 +0300

    Update NicknameModal.jsx

--- FrontEnd/src/components/NicknameModal.jsx
+++ FrontEnd/src/components/NicknameModal.jsx
@@ -14,7 +14,7 @@ const NicknameModal = ({ isOpen, onClose }) => {
     }
     
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', {
+      const response = await fetch('https://story-chain-jade.vercel.app/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
